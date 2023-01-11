737ffec159b1dd0a732efd7caf5d42902a8a34e4
ntua-el19923 <el19923@mail.ntua.gr>
2023-01-11 13:50:25 +0200

    Views Updated + added in both files

--- data/db_intelliq_draft/create_views_intelliq.sql
+++ data/db_intelliq_draft/create_views_intelliq.sql
@@ -1,16 +1,22 @@
-
+USE intelliqDB;
 /* view for questionnaire with possible answers */
 DROP VIEW IF EXISTS view_Questionnaire;
-CREATE VIEW view_Questionnaire AS 
-SELECT questionnaireTitle, qtext, opttxt
-FROM Questionnaire, Question, 'Option'
-WHERE Questionnaire.questionnaireID = Question.questionnaireID and 'Option'.qID = Question.qID;
+CREATE VIEW view_Questionnaire AS
+SELECT questionnaireTitle,qtext,opttxt
+FROM Questionnaire q
+         INNER JOIN Question a ON q.questionnaireID = a.questionnaireID
+         INNER JOIN `Option` b ON b.qID = a.qID
+GROUP BY q.questionnaireID;
+
+
 
 
 /* view session from user */
-DROP VIEW IF EXISTS view_Quview_Session_answersestionnaire;
+DROP VIEW IF EXISTS view_Session_answers;
 CREATE VIEW view_Session_answers AS
-SELECT Session.session, questionnaireTitle, qtext, opttxt
-FROM Questionnaire, Question, 'Option', Session, Answer
-WHERE Questionnaire.questionnaireID = Question.questionnaireID and 'Option'.qID = Question.qID and
-		Session.questionnaireID = Question.questionnaireID and 'Option'.optID = Answer.optID and Session.session = Answer.session
+SELECT s.session,questionnaireTitle,qtext,opttxt
+FROM Session s
+     INNER JOIN Questionnaire q ON s.questionnaireID = q.questionnaireID
+     INNER JOIN Answer a ON s.session = a.session
+     INNER JOIN `Option` o ON a.optID = o.optID
+     INNER JOIN Question ON Question.questionnaireID = q.questionnaireID AND o.qID = Question.qID;
--- data/db_intelliq_draft/intelliQ_schema_creation.sql
+++ data/db_intelliq_draft/intelliQ_schema_creation.sql
@@ -155,3 +155,28 @@ FLUSH PRIVILEGES;
 
 
 /*------------------------------------ ------------------------------------ ------------------------------------*/
+/*              VIEWS START HERE            */
+
+
+/* view for questionnaire with possible answers */
+DROP VIEW IF EXISTS view_Questionnaire;
+CREATE VIEW view_Questionnaire AS
+SELECT questionnaireTitle,qtext,opttxt
+FROM Questionnaire q
+         INNER JOIN Question a ON q.questionnaireID = a.questionnaireID
+         INNER JOIN `Option` b ON b.qID = a.qID
+GROUP BY q.questionnaireID;
+
+
+
+
+/* view session from user */
+DROP VIEW IF EXISTS view_Session_answers;
+CREATE VIEW view_Session_answers AS
+SELECT s.session,questionnaireTitle,qtext,opttxt
+FROM Session s
+    INNER JOIN Questionnaire q ON s.questionnaireID = q.questionnaireID
+    INNER JOIN Answer a ON s.session = a.session
+    INNER JOIN `Option` o ON a.optID = o.optID
+    INNER JOIN Question ON Question.questionnaireID = q.questionnaireID AND o.qID = Question.qID;
+/*              VIEWS END HERE            */
