67797e35d43b34ca2cbf8144fe8edc1edfa5308f
jcchatzis <johnco.chatzis@gmail.com>
2023-01-13 18:41:20 +0200

    users queries a --> e are up

---
+++ data/db_intelliq_draft/queries/user_queries/u_query3.sql
@@ -0,0 +1,3 @@
+/*          USER QUERY 2 --> c. {baseURL}/doanswer/:questionnaireID/:questionID/:session/:optionID     */
+
+INSERT INTO Answer (session, optID) VALUES ('{session}', '{optID}');
--- data/db_intelliq_draft/queries/user_queries/u_query_2.sql
+++ data/db_intelliq_draft/queries/user_queries/u_query_2.sql
@@ -1,3 +1,13 @@
 /*          USER QUERY 2 --> b. {baseURL}/question/:questionnaireID/:questionID     */
 
 
+SELECT
+    q.questionnaireID, q.qID, q.qtext, q.required, q.type,
+    o.optID, o.opttxt, o.nextqID
+FROM
+    Question q
+        JOIN `Option` O on o.qID = q.qID
+WHERE
+        q.questionnaireID = 'QQ001' AND
+        q.qID = 'P0015'
+order by o.optID
