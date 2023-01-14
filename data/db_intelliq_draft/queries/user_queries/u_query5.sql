b735f5805f1d7e716862c47470718578eb8158ed
LeKog <dimkog3301@yahoo.com>
2023-01-14 21:01:55 +0200

    Fixed query 5
    The joins with session and answer where the other way around and caused a big table to emerge which was wrong. Also removed the filtering by questionnaireID as it is not needed. Not it should work fine.

--- data/db_intelliq_draft/queries/user_queries/u_query5.sql
+++ data/db_intelliq_draft/queries/user_queries/u_query5.sql
@@ -1,14 +1,13 @@
 /*          USER QUERY 2=4 --> d. {baseURL}/getsessionanswers/:questionnaireID/:session     */
 
 SELECT
-    s.questionnaireID, o.qID, s.session, a.optID
+    q.questionnaireID, q.qID, s.session, a.optID
 FROM
     Question q
         INNER JOIN `Option` o ON q.qID = o.qID
-        INNER JOIN Session s ON s.questionnaireID = q.questionnaireID
         INNER JOIN Answer a ON a.optID = o.optID
+        INNER JOIN Session s ON a.session = s.session
 WHERE
-        q.questionnaireID = '{questionnaireID}' AND
         q.qID = '{qID}'
 ORDER BY
     s.session
