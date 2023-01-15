d47119f3f4aceb6e1841b7123b522b8ac7407dd5
LeKog <dimkog3301@yahoo.com>
2023-01-15 16:47:54 +0200

    Changes in queries
    Changed adming queries 3 and 4. Put ON DELETE CASCADE on Answer table FK session and deleted the useless create_views_intelliq file

--- data/db_intelliq_draft/queries/admin_queries/a_query4.sql
+++ data/db_intelliq_draft/queries/admin_queries/a_query4.sql
@@ -1,11 +1,4 @@
-				  
-4
+DELETE FROM Session where questionnaireID = "<<<>>>>";
+	  
 
-DELETE answer  from answer , session 
-where ['input'] = session.questionnaireID 
-      and session.session = answer.session
-	  
-	  
-DELETE  from  session 
-where ['input'] = session.questionnaireID 
 
