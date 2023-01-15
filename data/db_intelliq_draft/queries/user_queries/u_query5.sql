48d3c34b96a8d64295e9ae2e0a326facdcbf95c5
ntua-el19923 <el19923@mail.ntua.gr>
2023-01-15 16:59:05 +0200

    added timestamp in session + user query order by timestamp

--- data/db_intelliq_draft/queries/user_queries/u_query5.sql
+++ data/db_intelliq_draft/queries/user_queries/u_query5.sql
@@ -4,10 +4,10 @@ SELECT
     q.questionnaireID, q.qID, s.session, a.optID
 FROM
     Question q
-        INNER JOIN `Option` o ON q.qID = o.qID
+        INNER JOIN Option o ON q.qID = o.qID
         INNER JOIN Answer a ON a.optID = o.optID
         INNER JOIN Session s ON a.session = s.session
 WHERE
-        q.qID = '{qID}'
+    q.qID = '{qID}'
 ORDER BY
-    s.session
+    s.created_at
