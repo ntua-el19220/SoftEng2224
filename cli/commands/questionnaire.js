76fef0d2db1557eb0f9441a01e8d9d3c70603173
ntua-el19172 <94190385+ntua-el19172@users.noreply.github.com>
2023-02-14 18:22:26 +0200

    Added Cli

---
+++ cli/commands/questionnaire.js
@@ -0,0 +1,14 @@
+const axios = require ('axios');
+
+module.exports = function questionnaire_call(questionnaire_id1, format, token) {
+var questionnaire_id = '"'+questionnaire_id1+'"';
+
+
+    let url='https://localhost:9103/intelliq_api/questionnaire/'+questionnaire_id+'?format='+format;
+    axios.get(url, {
+        headers: { 
+        'X-OBSERVATORY-AUTH': token}
+      }).then( resp => {
+        console.log(resp.data);
+    });
+}
