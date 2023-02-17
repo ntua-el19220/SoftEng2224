46cb6a218af9e09e72648ff1eb7b7a56a9f60936
petrosrapto <petrosrapto@gmail.com>
2023-02-17 15:38:57 +0200

    createsession endpoint now generates valid sessionID

--- api-backend/createsession.js
+++ api-backend/createsession.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const pool = require('./connect');
 
 router.post('/:questionnaireID/:session', function(req, res) {
-    const {questionnaireID, session} = req.params;
+    let {questionnaireID, session} = req.params;
     const userID = req.decoded.userID;
 	pool.getConnection(function(err, connection) {
 		if(err) {
@@ -12,8 +12,8 @@ router.post('/:questionnaireID/:session', function(req, res) {
             console.log("Database Connection failed", err);
             res.end();
   		} else {
-            query_3 = `INSERT INTO Session (session, userID, questionnaireID) VALUES (${session}, ${userID}, ${questionnaireID});`;
-            connection.query(query_3, function(err, result) {
+            query = `SELECT randomsession();`;
+            connection.query(query, function(err, result) {
                 if(err) {
                     res.statusCode = 400;
                     res.statusMessage = "Bad Request";
@@ -21,14 +21,26 @@ router.post('/:questionnaireID/:session', function(req, res) {
                     connection.release();
                     res.end();
                 } else { 
-                    res.statusCode = 200;
-                    res.statusMessage = "OK";
-                    console.log("Session Created");
-                    // GET LAST SESSION INSERTED?
-                    res.status(200).json({message:"Session Created", session:session});
-                    connection.release();
-                    res.end();
-                    } 
+                    if (session == "\"\"") // if session is not given generate one
+                        session = '"'+result[0]['randomsession()']+'"';
+                    query_2 = `INSERT INTO Session (session, userID, questionnaireID) VALUES (${session}, ${userID}, ${questionnaireID});`;
+                    connection.query(query_2, function(err, result_2) {
+                        if(err) {
+                            res.statusCode = 400;
+                            res.statusMessage = "Bad Request";
+                            console.log("Bad request given", err);
+                            connection.release();
+                            res.end();
+                        } else { 
+                            res.statusCode = 200;
+                            res.statusMessage = "OK";
+                            console.log("Session Created");
+                            res.status(200).json({message:"Session Created", session:session});
+                            connection.release();
+                            res.end();
+                            } 
+                    });
+                } 
             });
         }
 	});
